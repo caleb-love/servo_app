59439974977df23b8765f8d723354bb85c95d22e
Caleb <30075010+caleb-love@users.noreply.github.com>
2023-04-03 14:27:54 +1000

    updating schema file

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const config = require("./config")
-const expressLayouts = require("express-ejs-layouts")
+const config = require('./config')
+const expressLayouts = require('express-ejs-layouts')
 
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 app.use(expressLayouts)
 app.use(express.urlencoded({ extended: true }))
-app.use(require("./middlewares/method_override"))
+app.use(require('./middlewares/method_override'))
 
-app.get("/", (req, res) => {
-  console.log(config.googleApiKey)
-  res.render("index", { googleApiKey: config.googleApiKey })
+app.get('/', (req, res) => {
+	console.log(config.googleApiKey)
+	res.render('index', { googleApiKey: config.googleApiKey })
 })
 
 app.listen(config.port, () => {
-  console.log(`listening on port ${config.port}`)
+	console.log(`listening on port ${config.port}`)
 })
