fbfd421473e50320ad1068ef28e6718d927f146f
Caleb <caleblove@live.com>
2023-04-03 17:21:53 +1000

    added clock function

--- public/components/clock.js
+++ public/components/clock.js
@@ -1,12 +1,11 @@
 function updateClock() {
-    const now = dayjs();
-    
-    const day = now.format('dddd');
-    const time = now.format('HH:mm:ss');
-  
-    const clockElement = document.querySelector('.clock');
-    clockElement.textContent = `${day} ${time}`;
-  }
-  
-  setInterval(updateClock, 1000);
+	const now = dayjs()
 
+	const day = now.format('dddd')
+	const time = now.format('HH:mm:ss')
+
+	const clockElement = document.querySelector('.clock')
+	clockElement.textContent = `${day} ${time}`
+}
+
+setInterval(updateClock, 1000)
