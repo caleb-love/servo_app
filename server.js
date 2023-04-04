4955e127e63ca11c370ddb19575130695969efe3
Caleb <caleblove@live.com>
2023-04-04 11:37:23 +1000

    spotlight

--- server.js
+++ server.js
@@ -15,30 +15,25 @@ app.use(express.json()) // json for body maybe not required ??
 const Station = require('./models/stations')
 
 app.get('/', (req, res) => {
-		res.render('index', { googleApiKey: config.googleApiKey })
+	res.render('index', { googleApiKey: config.googleApiKey })
 })
 
-app.get('/api/station/all', (req , res) => {
-	Station.findAll()
-	.then(dbRes => res.json(dbRes))
+app.get('/api/station/all', (req, res) => {
+	Station.findAll().then((dbRes) => res.json(dbRes))
 })
 
-
-app.get('/api/owners', (req ,res) => {
-	Station.total_stations()
-	.then( owners => res.json(owners))
+app.get('/api/owners', (req, res) => {
+	Station.total_stations().then((owners) => res.json(owners))
 })
 
-app.get('/api/stats', (req, res ) => {
-	Station.stats()
-	.then( stats => res.json(stats))
+app.get('/api/stats', (req, res) => {
+	Station.stats().then((stats) => res.json(stats))
 })
 
-app.get('/api/station/random', (req, res ) => {
+app.get('/api/station/random', (req, res) => {
 	Station.random_station()
-	.then(random => res.json(random))
-	})
-	
+		.then((random) => res.json(random))
+})
 
 app.listen(config.port, () => {
 	console.log(`listening on port ${config.port}`)
