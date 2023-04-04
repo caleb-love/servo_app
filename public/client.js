880b527a9505fea40ef6eca5b8197e7f2f835396
mo-ta-ali <tasi@live.com.au>
2023-04-04 12:50:31 +1000

    oil price works so far

--- public/client.js
+++ public/client.js
@@ -72,4 +72,43 @@ async function updatePetrolStationList() {
 }
 
 updatePetrolStationList()
+
+async function updateCommodityPrices() {
+
+	const dateElement = document.getElementById('current-date')
+	const wtiPriceElement = document.getElementById('wti-price')
+	const brentPriceElement = document.getElementById('brent-price')
+	const natgasPriceElement = document.getElementById('natgas-price')
   
+	const date = new Date().toLocaleDateString()
+	dateElement.textContent = `As of ${date}`
+
+	const apiKey = 'insert api key'
+	
+  
+	try {
+	  const response = await fetch(
+		`https://commodities-api.com/api/latest?access_key=${apiKey}&base=USD&symbols=WTIOIL,BRENTOIL,NG`,
+	  )
+  
+	  if (!response.ok) {
+		throw new Error('Unable to fetch commodity prices')
+	  }
+  
+	  const data = await response.json()
+  
+	  const wtiPrice = data.data.rates.WTIOIL.toFixed(2)
+	  wtiPriceElement.textContent = `$${wtiPrice}`
+  
+	  const brentPrice = data.data.rates.BRENTOIL.toFixed(2)
+	  brentPriceElement.textContent = `$${brentPrice}`
+  
+	  const natgasPrice = data.data.rates.NG.toFixed(2)
+	  natgasPriceElement.textContent = `$${natgasPrice}`
+	} catch (error) {
+	  console.error(error)
+	}
+
+}
+
+updateCommodityPrices()
