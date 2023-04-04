c64780232fd901ffa9b32c0cb24c2115972fca04
Caleb <caleblove@live.com>
2023-04-04 11:57:03 +1000

    spotlight

--- public/components/spotlight.js
+++ public/components/spotlight.js
@@ -1,8 +1,8 @@
 function getRandomStation() {
 	fetch('/api/station/random')
 		.then((response) => response.json())
-		.then((station) => {
-			const stationInfo = document.querySelector('#station-info')
-			stationInfo.innerHTML = `<p>Name: ${station.name}</p><p>Location: ${station.location}</p>`
+		.then((stations) => {
+			const spotlightStation = document.querySelector('#station-info')
+			spotlightStation.innerHTML = `<p>Name: ${stations.owner}</p><p>Location: ${stations.address}</p>`
 		})
 }
