0b7130f4ac8c37c48f86149b3289f6a305cef35e
squshedfrog <simonruc@yahoo.com.au>
2023-04-04 21:08:15 +1000

    auto populate stats

--- public/components/stats.js
+++ public/components/stats.js
@@ -1,2 +1,42 @@
-const stats = document.querySelector('.stats-section')
-stats.style.background
+
+const stats_list = document.querySelector('.stats-list')
+const total_stations = document.querySelector('.total-stations')
+const total_owners = document.querySelector('.total-owners')
+// stats_list.style.backgroundColor = 'red'
+
+let ownerStats 
+axios.get('/api/stats')
+.then(( res) => {
+	total_owners.textContent = res.data.total_owners
+	total_stations.textContent = res.data.total_stations
+	ownerStats = res.data.owners.slice(0,7)
+	stats_list.innerHTML = ownerStats
+		.map( (owner, i)=> {
+			return renderStats(owner , i)
+		})
+		.join('')	
+})	
+
+
+
+function renderStats(owner , i){
+	if ( i % 2 == 0 ){
+		return`
+			<li>
+				<div style="background-color: #ffe4c4;" class="station-item">
+		    		<span>${owner.owner}</span>
+					<span>${owner.count}</span>
+				</div>
+			</li>
+		`
+	} else {
+		return`
+			<li>
+				<div class="station-item">
+		    		<span>${owner.owner}</span>
+					<span>${owner.count}</span>
+				</div>
+			</li>
+		`
+	}
+}	
