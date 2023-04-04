7c23cd0005ce56d5f6b93973bad7a9b7a80d3321
squshedfrog <simonruc@yahoo.com.au>
2023-04-04 21:08:15 +1000

    auto populate stats

--- public/components/spotlight.js
+++ public/components/spotlight.js
@@ -1,9 +1,15 @@
+
+
 function getRandomStation() {
 	fetch('/api/station/random')
 		.then((response) => response.json())
 		.then((stations) => {
 			const spotlightStation = document.querySelector('#station-info')
-			spotlightStation.innerHTML = `<p>Name: ${stations.owner}</p><p>Location: ${stations.address}</p>`
+			spotlightStation.innerHTML = `<p>Name: ${stations.name}</p><p>Location: ${stations.address}</p>`
+
+			const spotlight_right =document.querySelector('.spotlight-right')
+			const img = spotlight_right.querySelector('img')
+			img.src=stations.logo
 		})
 }
 
