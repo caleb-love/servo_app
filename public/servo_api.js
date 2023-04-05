88bc06e151f96bf5ae8288353fcf030c074f0a61
Nikki Pham <nikki.piknik@gmail.com>
2023-04-05 16:52:06 +1000

    changed fetch to axios and put in servo api for getRandomStation

--- public/servo_api.js
+++ public/servo_api.js
@@ -6,3 +6,7 @@ export function fetchStationsInBound(southLat, northLat, westLng, eastLng) {
     return axios.get(`/api/station/bounds?south=${southLat}&north=${northLat}&west=${westLng}&east=${eastLng}`)
         .then(res => res.data)
 }
+
+export function fetchRandom() {
+    return axios.get('/api/station/random').then((res) => res.data)
+}
