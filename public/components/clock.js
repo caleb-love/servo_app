dc7df9ae52e927f5caa6396851b341e7aa3d3df4
Caleb <caleblove@live.com>
2023-04-05 16:16:05 +1000

    bug fixes and performance improvements

--- public/components/clock.js
+++ public/components/clock.js
@@ -1,11 +1,15 @@
-function updateClock() {
-	const now = dayjs()
-
+function formatTime(now) {
 	const day = now.format('dddd')
 	const time = now.format('HH:mm:ss')
+	return `${day} ${time}`
+}
 
+function updateClock() {
+	const now = dayjs()
+	const formattedTime = formatTime(now)
 	const clockElement = document.querySelector('.time')
-	clockElement.textContent = `${day} ${time}`
+
+	clockElement.textContent = formattedTime
 }
 
 setInterval(updateClock, 1000)
