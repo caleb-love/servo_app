ab5049706292958800b64326af167a35d7bcfc90
squshedfrog <simonruc@yahoo.com.au>
2023-04-05 16:50:32 +1000

    refactor server.js

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
 const config = require('./config')
-const db = require('./db')
 const Station = require('./models/stations')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-app.use(require('./middlewares/method_override'))
-app.use(express.json())
+
+
 
 app.get('/', (req, res) => {
 	res.render('index', {
