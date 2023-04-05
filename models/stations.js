3befe3cdd1ac6fb6fce2dbac6001dc8ed59465d4
Caleb <30075010+caleb-love@users.noreply.github.com>
2023-04-05 16:16:05 +1000

    bug fixes and performance improvements

--- models/stations.js
+++ models/stations.js
@@ -1,55 +1,58 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const db =require('../db')
+const db = require('../db')
 
 class Station {
+	static findAll() {
+		const sql = 'select * from stations limit 400;'
+		//const sql = "select * from stations where state = 'New South Wales';"
+		return db.query(sql).then((res) => res.rows)
+	}
 
-    static findAll(){
-        const sql = "select * from stations limit 400;"
-        //const sql = "select * from stations where state = 'New South Wales';"
-        return db.query(sql).then(res => res.rows )
-    }
-
-    static total_stations(){
-        const sql = "select distinct owner from stations;"
-        return db.query(sql)
-	    .then( dbRes => { 
-		    let owners = (dbRes.rows.map((owner) => owner.owner ))
-		    let count = dbRes.rows.length
-		    return {owners : owners, total_owners : count}
+	static total_stations() {
+		const sql = 'select distinct owner from stations;'
+		return db.query(sql).then((dbRes) => {
+			let owners = dbRes.rows.map((owner) => owner.owner)
+			let count = dbRes.rows.length
+			return { owners: owners, total_owners: count }
 		})
-    }
+	}
 
-    static random_station(){
-        const sql = 'select * from stations order by random() limit 1;'
-	    return db.query(sql)
-	    .then( dbRes => dbRes.rows[0] )
-    }
+	static async random_station() {
+		const countRes = await db.query('SELECT COUNT(*) FROM stations')
+		const count = countRes.rows[0].count
+		const offset = Math.floor(Math.random() * count)
+		const sql = 'SELECT * FROM stations LIMIT 1 OFFSET $1'
+		const res = await db.query(sql, [offset])
+		return res.rows[0]
+	}
 
-    static in_bounds(southLat, northLat, westLng, eastLng) {
-        const sql = 'select * from stations where latitude between $1 and $2 and longitude between $3 and $4;'
-        return db.query(sql, [southLat, northLat, westLng, eastLng]).then(res => res.rows )
-    }
-    
-    static stats(){
-               
-        const sql1 = 'select owner, count(owner) from stations group by owner having count (owner) > 1 order by count desc;'
-	    const sql2 = 'select count(*) as total_stations, count(distinct owner) as total_owners from stations;'
-	    	
-	    let owners = db.query(sql1)
-	    let total_stations = db.query(sql2)
-        return Promise.all([owners , total_stations])	
-        .then( dbRes => {
-            let owners = dbRes[0].rows
-            let total_owners = dbRes[1].rows[0].total_owners
-            let total_stations = dbRes[1].rows[0].total_stations
-            return {owners , total_owners , total_stations }
-    })
-    }
+	static in_bounds(southLat, northLat, westLng, eastLng) {
+		const sql =
+			'select * from stations where latitude between $1 and $2 and longitude between $3 and $4;'
+		return db
+			.query(sql, [southLat, northLat, westLng, eastLng])
+			.then((res) => res.rows)
+	}
 
-    static stations_nearby(lat, lng, radius) {
-        
-        const query = `
+	static stats() {
+		const sql1 =
+			'select owner, count(owner) from stations group by owner having count (owner) > 1 order by count desc;'
+		const sql2 =
+			'select count(*) as total_stations, count(distinct owner) as total_owners from stations;'
+
+		let owners = db.query(sql1)
+		let total_stations = db.query(sql2)
+		return Promise.all([owners, total_stations]).then((dbRes) => {
+			let owners = dbRes[0].rows
+			let total_owners = dbRes[1].rows[0].total_owners
+			let total_stations = dbRes[1].rows[0].total_stations
+			return { owners, total_owners, total_stations }
+		})
+	}
+
+	static stations_nearby(lat, lng, radius) {
+		const query = `
           SELECT id, name, owner, address, logo, latitude, longitude,
             earth_distance(ll_to_earth($1, $2), ll_to_earth(latitude, longitude)) AS distance
           FROM stations
@@ -57,10 +60,10 @@ class Station {
           ORDER BY distance
           LIMIT 700
         `
-        return db.query(query, [lat, lng, radius * 1000]).then(res => res.rows)
-    }
-
+		return db
+			.query(query, [lat, lng, radius * 1000])
+			.then((res) => res.rows)
+	}
 }
-
 
 module.exports = Station
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
 const config = require('./config')
-const expressLayouts = require('express-ejs-layouts')
 const db = require('./db')
-
-app.set('view engine', 'ejs')
-
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-
-app.use(require('./middlewares/method_override'))
-app.use(express.json()) // json for body maybe not required ??
 const Station = require('./models/stations')
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(require('./middlewares/method_override'))
+app.use(express.json())
+
 app.get('/', (req, res) => {
-	res.render('index', { googleApiKey: config.googleApiKey, commoditiesApiKey: process.env.COMMODITIES_API_KEY, })
+	res.render('index', {
+		googleApiKey: config.googleApiKey,
+		commoditiesApiKey: process.env.COMMODITIES_API_KEY,
+	})
 })
 
 app.get('/api/station/all', (req, res) => {
@@ -30,48 +30,51 @@ app.get('/api/stats', (req, res) => {
 })
 
 app.get('/api/station/random', (req, res) => {
-	Station.random_station()
-		.then((random) => res.json(random))
+	Station.random_station().then((random) => res.json(random))
 })
 
 app.get('/api/station/bounds', (req, res) => {
-
-	Station.in_bounds(req.query.south, req.query.north, req.query.west, req.query.east)
-		.then((stations) => res.json(stations))
+	const { south, north, west, east } = req.query
+	Station.in_bounds(south, north, west, east).then((stations) =>
+		res.json(stations)
+	)
 })
 
 app.get('/api/stations/nearest', async (req, res) => {
-
 	const latitude = parseFloat(req.query.latitude)
 	const longitude = parseFloat(req.query.longitude)
 	const radius = parseFloat(req.query.radius)
 	const limit = 700
 
 	const query = `
-	  SELECT *, 
-		(6371 * acos(
-		  cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude))
-		)) AS distance
-	  FROM stations
-	  WHERE 
-		(6371 * acos(
-		  cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude))
-		)) <= $3
-	  ORDER BY distance
-	  LIMIT $4;
-	`
-  
+    SELECT *, 
+      (6371 * acos(
+        cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude))
+      )) AS distance
+    FROM stations
+    WHERE 
+      (6371 * acos(
+        cos(radians($1)) * cos(radians(latitude)) * cos(radians(longitude) - radians($2)) + sin(radians($1)) * sin(radians(latitude))
+      )) <= $3
+    ORDER BY distance
+    LIMIT $4;
+  `
+
 	try {
-	  const result = await db.query(query, [latitude, longitude, radius, limit])
-	  res.json(result.rows)
-	  
+		const result = await db.query(query, [
+			latitude,
+			longitude,
+			radius,
+			limit,
+		])
+		res.json(result.rows)
 	} catch (error) {
-	  console.error(error)
-	  res.status(500).json({ error: 'An error occurred while fetching nearest stations' })
+		console.error(error)
+		res.status(500).json({
+			error: 'An error occurred while fetching nearest stations',
+		})
 	}
-
 })
-
 
 app.listen(config.port, () => {
 	console.log(`listening on port ${config.port}`)
