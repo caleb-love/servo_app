3d6c8d66cd962a58656113b67365a226b206216a
Nikki Pham <nikki.piknik@gmail.com>
2023-04-06 11:50:21 +1000

    back button styling

--- public/components/map.js
+++ public/components/map.js
@@ -225,7 +225,7 @@ async function initMap() {
 			const backButton = document.createElement('button')
 			backButton.textContent = 'Current Location'
 			backButton.classList.add('back-button')
-			map.controls[google.maps.ControlPosition.RIGHT_TOP].push(
+			map.controls[google.maps.ControlPosition.LEFT_TOP].push(
 				backButton
 			)
 
