2254a160be46370379d8686f15cf69a27c1cae06
Caleb <caleblove@live.com>
2023-04-06 15:21:17 +1000

    epilepsy mode

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-let missing = ['PORT', 'DATABASE_URL', 'GOOGLE_API_KEY'].filter(
+let missing = ['PORT', 'DATABASE_URL', 'GOOGLE_API_KEY', 'COMMODITIES_API_KEY'].filter(
 	(key) => process.env[key] === undefined
 )
 
